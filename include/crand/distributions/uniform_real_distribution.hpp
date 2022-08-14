b4997fb7b00226d9ddc03ff5b48ddfe9e8baf356
Jan Möller <drag-on@gmx.de>
2022-08-14 13:46:53 +0200

    Implement uniform_real_distribution

---
+++ include/crand/distributions/uniform_real_distribution.hpp
@@ -0,0 +1,136 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef CONSTEXPR_RANDOM_UNIFORM_REAL_DISTRIBUTION_HPP
+#define CONSTEXPR_RANDOM_UNIFORM_REAL_DISTRIBUTION_HPP
+
+#include "detail/uniform_real_distribution_details.hpp"
+#include "distribution_limits.hpp"
+#include "uniform_int_distribution.hpp"
+
+#include <random>
+
+#include <cassert>
+#include <cmath>
+
+namespace crand
+{
+template<class RealType = double>
+class uniform_real_distribution
+{
+  public:
+    using result_type = RealType;
+
+    constexpr uniform_real_distribution(inclusive<RealType> a, inclusive<RealType> b) noexcept
+        : m_a(a.value)
+        , m_b(b.value)
+        , m_g(detail::uniform_real_distribution::compute_gamma(m_a, m_b))
+        , m_hi(m_g > 0 ? detail::uniform_real_distribution::ceilint(m_a, m_b, m_g) : 0)
+        , m_min(m_a)
+        , m_max(m_b)
+        , m_int_dist(0, m_hi)
+        , m_gn(std::abs(m_a) <= std::abs(m_b)
+                   ? detail::uniform_real_distribution::gen_inclusive_inclusive_loe<RealType>
+                   : detail::uniform_real_distribution::gen_inclusive_inclusive_nloe<RealType>)
+    {
+        assert(m_b - m_a <= std::numeric_limits<RealType>::max());
+        assert(m_a <= m_b);
+    }
+    constexpr uniform_real_distribution(exclusive<RealType> a, inclusive<RealType> b) noexcept
+        : m_a(a.value)
+        , m_b(b.value)
+        , m_g(detail::uniform_real_distribution::compute_gamma(m_a, m_b))
+        , m_hi(detail::uniform_real_distribution::ceilint(m_a, m_b, m_g))
+        , m_min(std::abs(m_a) <= std::abs(m_b) ? m_b - (m_hi - 1) * m_g : m_a + m_g)
+        , m_max(m_b)
+        , m_int_dist(0, m_hi - 1)
+        , m_gn(std::abs(m_a) <= std::abs(m_b)
+                   ? detail::uniform_real_distribution::gen_exclusive_inclusive_loe<RealType>
+                   : detail::uniform_real_distribution::gen_exclusive_inclusive_nloe<RealType>)
+    {
+        assert(m_b - m_a <= std::numeric_limits<RealType>::max());
+        assert(m_a < m_b);
+    }
+    constexpr uniform_real_distribution(inclusive<RealType> a, exclusive<RealType> b) noexcept
+        : m_a(a.value)
+        , m_b(b.value)
+        , m_g(detail::uniform_real_distribution::compute_gamma(m_a, m_b))
+        , m_hi(detail::uniform_real_distribution::ceilint(m_a, m_b, m_g))
+        , m_min(m_a)
+        , m_max(std::abs(m_a) <= std::abs(m_b) ? m_b - m_g : m_a + (m_hi - 1) * m_g)
+        , m_int_dist(1, m_hi)
+        , m_gn(std::abs(m_a) <= std::abs(m_b)
+                   ? detail::uniform_real_distribution::gen_inclusive_exclusive_loe<RealType>
+                   : detail::uniform_real_distribution::gen_inclusive_exclusive_nloe<RealType>)
+    {
+        assert(m_b - m_a <= std::numeric_limits<RealType>::max());
+        assert(m_a < m_b);
+    }
+    constexpr uniform_real_distribution(exclusive<RealType> a, exclusive<RealType> b) noexcept
+        : m_a(a.value)
+        , m_b(b.value)
+        , m_g(detail::uniform_real_distribution::compute_gamma(m_a, m_b))
+        , m_hi(detail::uniform_real_distribution::ceilint(m_a, m_b, m_g))
+        , m_min(std::abs(m_a) <= std::abs(m_b) ? m_b - (m_hi - 1) * m_g : m_a + m_g)
+        , m_max(std::abs(m_a) <= std::abs(m_b) ? m_b - m_g : m_a + (m_hi - 1) * m_g)
+        , m_int_dist(1, m_hi - 1)
+        , m_gn(std::abs(m_a) <= std::abs(m_b)
+                   ? detail::uniform_real_distribution::gen_exclusive_exclusive_loe<RealType>
+                   : detail::uniform_real_distribution::gen_exclusive_exclusive_nloe<RealType>)
+    {
+        assert(m_b - m_a <= std::numeric_limits<RealType>::max());
+        assert(m_a < m_b);
+//        assert(std::nexttoward(m_a, m_b) != m_b); // TODO: check why it doesn't compile
+    }
+
+    template<std::uniform_random_bit_generator G>
+    constexpr auto operator()(G& g) const -> result_type
+    {
+        auto const k = m_int_dist(g);
+        return m_gn(m_a, m_b, m_g, m_hi, k);
+    }
+
+    [[nodiscard]] constexpr auto a() const noexcept -> result_type { return m_a; }
+    [[nodiscard]] constexpr auto b() const noexcept -> result_type { return m_b; }
+    [[nodiscard]] constexpr auto min() const noexcept -> result_type { return m_min; }
+    [[nodiscard]] constexpr auto max() const noexcept -> result_type { return m_max; }
+    [[nodiscard]] constexpr auto gamma() const noexcept -> result_type { return m_g; }
+    [[nodiscard]] constexpr auto num_unique_values() const noexcept -> std::size_t
+    {
+        return m_int_dist.max() - m_int_dist.min();
+    }
+
+  private:
+    RealType                              m_a;
+    RealType                              m_b;
+    RealType                              m_g;
+    std::size_t                           m_hi;
+    RealType                              m_min;
+    RealType                              m_max;
+    uniform_int_distribution<std::size_t> m_int_dist;
+    RealType (*m_gn)(RealType a, RealType b, RealType g, RealType hi, std::size_t k);
+};
+} // namespace crand
+
+#endif // CONSTEXPR_RANDOM_UNIFORM_REAL_DISTRIBUTION_HPP
---
+++ test/distributions/test_uniform_real_distribution.cpp
@@ -0,0 +1,202 @@
+//
+// MIT License
+//
+// Copyright (c) 2022 Jan Möller
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include <bugspray/bugspray.hpp>
+#include <crand/distributions/uniform_real_distribution.hpp>
+#include <crand/engines/xoshiro256_starstar_engine.hpp>
+
+TEST_CASE("uniform_real_distribution", "[distributions]")
+{
+    using namespace crand;
+    xoshiro256_starstar e;
+
+    int const runs = std::is_constant_evaluated() ? 1000 : 100000;
+
+    SECTION("inclusive - inclusive")
+    {
+        SECTION("range [0,1]")
+        {
+            double const              a = 0.;
+            double const              b = 1.;
+            uniform_real_distribution d(inclusive{a}, inclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a);
+            REQUIRE(d.max() == b);
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k >= a);
+                REQUIRE(k <= b);
+            }
+        }
+
+        SECTION("range [42, 42]")
+        {
+            double const              a = 42.;
+            double const              b = 42.;
+            uniform_real_distribution d(inclusive{a}, inclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a);
+            REQUIRE(d.max() == b);
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k == a);
+            }
+        }
+
+        SECTION("range [0,max]")
+        {
+            double const              a = 0.;
+            double const              b = std::numeric_limits<double>::max();
+            uniform_real_distribution d(inclusive{a}, inclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a);
+            REQUIRE(d.max() == b);
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k >= a);
+                REQUIRE(k <= b);
+            }
+        }
+    }
+    SECTION("exclusive - inclusive")
+    {
+        SECTION("range (0,1]")
+        {
+            double const              a = 0.;
+            double const              b = 1.;
+            uniform_real_distribution d(exclusive{a}, inclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a + d.gamma());
+            REQUIRE(d.max() == b);
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k > a);
+                REQUIRE(k <= b);
+            }
+        }
+        SECTION("range (0,max]")
+        {
+            double const              a = 0.;
+            double const              b = std::numeric_limits<double>::max();
+            uniform_real_distribution d(exclusive{a}, inclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a + d.gamma());
+            REQUIRE(d.max() == b);
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k > a);
+                REQUIRE(k <= b);
+            }
+        }
+    }
+    SECTION("inclusive - exclusive")
+    {
+        SECTION("range [0, 1)")
+        {
+            double const              a = 0.;
+            double const              b = 1.;
+            uniform_real_distribution d(inclusive{a}, exclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a);
+            REQUIRE(d.max() == b - d.gamma());
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k >= a);
+                REQUIRE(k < b);
+            }
+        }
+        SECTION("range [0,max)")
+        {
+            double const              a = 0.;
+            double const              b = std::numeric_limits<double>::max();
+            uniform_real_distribution d(inclusive{a}, exclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a);
+            REQUIRE(d.max() == b - d.gamma());
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k >= a);
+                REQUIRE(k < b);
+            }
+        }
+    }
+    SECTION("exclusive - exclusive")
+    {
+        SECTION("range (0,1)")
+        {
+            double const              a = 0.;
+            double const              b = 1.;
+            uniform_real_distribution d(exclusive{a}, exclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a + d.gamma());
+            REQUIRE(d.max() == b - d.gamma());
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k > a);
+                REQUIRE(k < b);
+            }
+        }
+        SECTION("range (0,max)")
+        {
+            double const              a = 0.;
+            double const              b = std::numeric_limits<double>::max();
+            uniform_real_distribution d(exclusive{a}, exclusive{b});
+            REQUIRE(d.a() == a);
+            REQUIRE(d.b() == b);
+            REQUIRE(d.min() == a + d.gamma());
+            REQUIRE(d.max() == b - d.gamma());
+
+            for (int i = 0; i < runs; ++i)
+            {
+                auto const k = d(e);
+                REQUIRE(k > a);
+                REQUIRE(k < b);
+            }
+        }
+    }
+}
+EVAL_TEST_CASE("uniform_real_distribution");
